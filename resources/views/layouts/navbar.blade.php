d58c35ede897469b668f03361297dbe24969c8a6
THANETNove <novel2020manga@gmail.com>
2024-06-28 12:36:46 +0700

    add

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,9 @@
 <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">projecttorSiam<span>.</span></a>
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ URL::asset('/assets/images/cover/1.jpg') }}" class="imageLogo">
+        </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
             aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,60 +15,150 @@
                 <li class="nav-item @if (Request::is('/')) active @endif">
                     <a class="nav-link" href="{{ url('/') }}">Home</a>
                 </li>
-                <li class="nav-item @if (Request::is('shop')) active @endif">
+                {{--    <li class="nav-item @if (Request::is('shop')) active @endif">
                     <a class="nav-link" href="{{ url('shop') }}">Shop</a>
-                </li>
-                <li class="nav-item @if (Request::is('aboutus')) active @endif">
-                    <a class="nav-link" href="{{ url('aboutus') }}">About us</a>
-                </li>
-                <li class="nav-item @if (Request::is('services')) active @endif">
-                    <a class="nav-link" href="{{ url('services') }}">Services</a>
-                </li>
-                <li class="nav-item @if (Request::is('policy')) active @endif">
-                    <a class="nav-link" href="{{ url('policy') }}">Policy</a>
-                </li>
-                <li class="nav-item @if (Request::is('contact')) active @endif">
-                    {{-- <a class="nav-link" href="contact.html">Contact us</a> --}}
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item @if (Request::is('login')) active @endif">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li> --}}
+                <li class="nav-item dropdown @if (Request::is('shop/*')) active @endif">
+                    <a class="nav-link dropdown-toggle" href="#" id="policyDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Projector
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="policyDropdown">
+                        <!-- เริ่มต้น row -->
+                        <div class="row">
+                            <!-- คอลัมน์แรก -->
+                            <div class="col-4">
+                                <li><a class="nav-link" href="{{ url('shop/privacy') }}">Privacy Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/terms') }}">Terms of Service</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/cookie') }}">Cookie Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/security') }}">Security Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/accessibility') }}">Accessibility</a>
                                 </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item  @if (Request::is('register')) active @endif">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->fname }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                            </div>
+                            <!-- คอลัมน์ที่สอง -->
+                            <div class="col-4">
+                                <li><a class="nav-link" href="{{ url('shop/returns') }}">Returns Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/shipping') }}">Shipping Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/support') }}">Support</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/contact') }}">Contact Us</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/faq') }}">FAQ</a></li>
+                            </div>
+                            <!-- คอลัมน์ที่สอง -->
+                            <div class="col-4">
+                                <li><a class="nav-link" href="{{ url('shop/returns') }}">Returns Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/shipping') }}">Shipping Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/support') }}">Support</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/contact') }}">Contact Us</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/faq') }}">FAQ</a></li>
+                            </div>
+                        </div>
                     </ul>
                 </li>
+                <li class="nav-item dropdown @if (Request::is('shop/*')) active @endif">
+                    <a class="nav-link dropdown-toggle" href="#" id="policyDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        screen
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="policyDropdown">
+                        <!-- เริ่มต้น row -->
+                        <div class="row">
+                            <!-- คอลัมน์แรก -->
+                            <div class="col-4">
+                                <li><a class="nav-link" href="{{ url('shop/privacy') }}">Privacy Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/terms') }}">Terms of Service</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/cookie') }}">Cookie Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/security') }}">Security Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/accessibility') }}">Accessibility</a>
+                                </li>
+                            </div>
+                            <!-- คอลัมน์ที่สอง -->
+                            <div class="col-4">
+                                <li><a class="nav-link" href="{{ url('shop/returns') }}">Returns Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/shipping') }}">Shipping Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/support') }}">Support</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/contact') }}">Contact Us</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/faq') }}">FAQ</a></li>
+                            </div>
+                            <!-- คอลัมน์ที่สอง -->
+                            <div class="col-4">
+                                <li><a class="nav-link" href="{{ url('shop/returns') }}">Returns Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/shipping') }}">Shipping Policy</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/support') }}">Support</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/contact') }}">Contact Us</a></li>
+                                <li><a class="nav-link" href="{{ url('shop/faq') }}">FAQ</a></li>
+                            </div>
+                        </div>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown @if (Request::is('displays/*')) active @endif">
+                    <a class="nav-link dropdown-toggle" href="#" id="policyDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Displays
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="displaysDropdown">
+                        <li><a class="nav-link" href="{{ url('displays/privacy') }}">Privacy Policy</a></li>
+                        <li><a class="nav-link" href="{{ url('displays/terms') }}">Terms of Service</a></li>
+                        <li><a class="nav-link" href="{{ url('displays/cookie') }}">Cookie Policy</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown @if (Request::is('pages/*')) active @endif">
+                    <a class="nav-link dropdown-toggle" href="#" id="policyDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        PAGES
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="pagesDropdown">
+                        <li><a class="nav-link" href="{{ url('pages/privacy') }}">Privacy Policy</a></li>
+                        <li><a class="nav-link" href="{{ url('pages/terms') }}">Terms of Service</a></li>
+                        <li><a class="nav-link" href="{{ url('pages/cookie') }}">Cookie Policy</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown @if (Request::is('services/*')) active @endif">
+                    <a class="nav-link dropdown-toggle" href="#" id="policyDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Services
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
+                        <li><a class="nav-link" href="{{ url('services/aboutus') }}">About us</a></li>
+                        <li><a class="nav-link" href="{{ url('services/services') }}">Services</a></li>
+
+                    </ul>
+                </li>
+
             </ul>
 
-            <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-                <li><a class="nav-link" href="cart.html"><img src="{{ URL::asset('/assets/images/cart.svg') }}">
+            <ul class="custom-navbar-nav navbar-nav">
+                <li class="nav-item dropdown @if (Request::is('user/*')) active @endif">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="{{ URL::asset('/assets/images/user.svg') }}" alt="User Icon">
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="userDropdown">
+                        @if (auth()->check() && auth()->user())
+                            <li><a class="nav-link" href="#"> {{ Auth::user()->fname }}</a></li>
+                        @endif
+
+                        <li><a class="nav-link" href="{{ url('settings') }}">Settings</a></li>
+                        <li> <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                        <li> <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                        @if (auth()->check() && auth()->user())
+                            <li>
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        @endif
+                    </ul>
+                </li>
+
+
+                <li><a class="nav-link" href="#"><img src="{{ URL::asset('/assets/images/cart.svg') }}">
                         <span class="cartUser">5</span>
                     </a>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,137 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-    {{--     <div class="hero">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="col-lg-5">
-                    <div class="intro-excerpt">
-                        <h1>Modern Interior <span clsas="d-block">Design Studio</span></h1>
-                        <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-                            vulputate velit imperdiet dolor tempor tristique.</p>
-                        <p><a href="" class="btn btn-secondary me-2">Shop Now</a><a href="#"
-                                class="btn btn-white-outline">Explore</a></p>
-                    </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="hero-img-wrap">
-                        <img src="{{ URL::asset('/assets/images/cover/EpsonEF-11_EF12-5.jpg') }}" class="img-fluid">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-    <div class="testimonial-section">
-        <div class="container">
+    <div class="hero2">
 
-            <div class="row justify-content-center">
-                <div class="col-lg-12">
-                    <div class="testimonial-slider-wrap text-center">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="testimonial-slider-wrap text-center">
 
-                        <div id="testimonial-nav" aria-label="Carousel Navigation" tabindex="0">
-                            <span class="prev" data-controls="prev" aria-controls="tns1" tabindex="-1"><span
-                                    class="fa fa-chevron-left"></span></span>
-                            <span class="next" data-controls="next" aria-controls="tns1" tabindex="-1"><span
-                                    class="fa fa-chevron-right"></span></span>
+                    <div id="testimonial-nav" aria-label="Carousel Navigation" tabindex="0">
+                        <span class="prev" data-controls="prev" aria-controls="tns1" tabindex="-1"><span
+                                class="fa fa-chevron-left"></span></span>
+                        <span class="next" data-controls="next" aria-controls="tns1" tabindex="-1"><span
+                                class="fa fa-chevron-right"></span></span>
+                    </div>
+
+                    <div class="tns-outer" id="tns1-ow">
+                        <div class="tns-nav" aria-label="Carousel Pagination"><button data-nav="0" aria-controls="tns1"
+                                style="" aria-label="Carousel Page 1 (Current Slide)"
+                                class="tns-nav-active"></button><button data-nav="1" aria-controls="tns1" style=""
+                                aria-label="Carousel Page 2" class="" tabindex="-1"></button><button data-nav="2"
+                                aria-controls="tns1" style="" aria-label="Carousel Page 3" class=""
+                                tabindex="-1"></button></div>
+                        <div class="tns-liveregion tns-visually-hidden" aria-live="polite" aria-atomic="true">slide
+                            <span class="current">5</span> of 3
                         </div>
+                        <div id="tns1-mw" class="tns-ovh">
+                            <div class="tns-inner" id="tns1-iw">
+                                <div class="testimonial-slider  tns-slider tns-carousel tns-subpixel tns-calc tns-horizontal"
+                                    id="tns1" style="transform: translate3d(-80%, 0px, 0px);">
+                                    <div class="item tns-item" aria-hidden="true" tabindex="-1">
+                                        <div class="row justify-content-center">
+                                            <div class="col-lg-12 mx-auto">
 
-                        <div class="tns-outer" id="tns1-ow">
-                            <div class="tns-nav" aria-label="Carousel Pagination"><button data-nav="0"
-                                    aria-controls="tns1" style="" aria-label="Carousel Page 1 (Current Slide)"
-                                    class="tns-nav-active"></button><button data-nav="1" aria-controls="tns1"
-                                    style="" aria-label="Carousel Page 2" class=""
-                                    tabindex="-1"></button><button data-nav="2" aria-controls="tns1" style=""
-                                    aria-label="Carousel Page 3" class="" tabindex="-1"></button></div>
-                            <div class="tns-liveregion tns-visually-hidden" aria-live="polite" aria-atomic="true">slide
-                                <span class="current">5</span> of 3
-                            </div>
-                            <div id="tns1-mw" class="tns-ovh">
-                                <div class="tns-inner" id="tns1-iw">
-                                    <div class="testimonial-slider  tns-slider tns-carousel tns-subpixel tns-calc tns-horizontal"
-                                        id="tns1" style="transform: translate3d(-80%, 0px, 0px);">
-                                        <div class="item tns-item" aria-hidden="true" tabindex="-1">
-                                            <div class="row justify-content-center">
-                                                <div class="col-lg-12 mx-auto">
-
-                                                    <div class="testimonial-block text-center row">
-                                                        <blockquote class="mb-5 col-6">
-                                                            <p>“Donec facilisis quam ut purus rutrum lobortis. Donec vitae
-                                                                odio quis nisl dapibus malesuada. Nullam ac aliquet velit.
-                                                                Aliquam vulputate velit imperdiet dolor tempor tristique.
-                                                                Pellentesque habitant morbi tristique senectus et netus et
-                                                                malesuada fames ac turpis egestas. Integer convallis
-                                                                volutpat dui quis scelerisque.”</p>
-                                                        </blockquote>
-
-                                                        <div class="author-info col-6">
-                                                            <img src="{{ URL::asset('/assets/images/cover/banner-image.png') }}"
-                                                                class="bannerImage" alt="Maria Jones">
-
-                                                        </div>
-                                                    </div>
-
-                                                </div>
+                                                <img src="{{ URL::asset('/assets/images/cover/2.jpg') }}"
+                                                    class="bannerImage" alt="Maria Jones">
                                             </div>
                                         </div>
-                                        <!-- item tns-ite  end -->
-                                        <div class="item tns-item" aria-hidden="true" tabindex="-1">
-                                            <div class="row justify-content-center">
-                                                <div class="col-lg-12 mx-auto">
-
-                                                    <div class="testimonial-block text-center row">
-                                                        <blockquote class="mb-5 col-6">
-                                                            <p>“Donec facilisis quam ut purus rutrum lobortis. Donec vitae
-                                                                odio quis nisl dapibus malesuada. Nullam ac aliquet velit.
-                                                                Aliquam vulputate velit imperdiet dolor tempor tristique.
-                                                                Pellentesque habitant morbi tristique senectus et netus et
-                                                                malesuada fames ac turpis egestas. Integer convallis
-                                                                volutpat dui quis scelerisque.”</p>
-                                                        </blockquote>
-
-                                                        <div class="author-info col-6">
-                                                            <img src="{{ URL::asset('/assets/images/cover/banner-image.png') }}"
-                                                                class="bannerImage" alt="Maria Jones">
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <!-- item tns-ite  end -->
-                                        <div class="item tns-item" aria-hidden="true" tabindex="-1">
-                                            <div class="row justify-content-center">
-                                                <div class="col-lg-12 mx-auto">
-
-                                                    <div class="testimonial-block text-center row">
-                                                        <blockquote class="mb-5 col-6">
-                                                            <p>“Donec facilisis quam ut purus rutrum lobortis. Donec vitae
-                                                                odio quis nisl dapibus malesuada. Nullam ac aliquet velit.
-                                                                Aliquam vulputate velit imperdiet dolor tempor tristique.
-                                                                Pellentesque habitant morbi tristique senectus et netus et
-                                                                malesuada fames ac turpis egestas. Integer convallis
-                                                                volutpat dui quis scelerisque.”</p>
-                                                        </blockquote>
-
-                                                        <div class="author-info col-6">
-                                                            <img src="{{ URL::asset('/assets/images/cover/banner-image.png') }}"
-                                                                class="bannerImage" alt="Maria Jones">
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!-- item tns-ite  end -->
                                     </div>
+                                    <!-- item tns-ite  end -->
+                                    <div class="item tns-item" aria-hidden="true" tabindex="-1">
+                                        <div class="row justify-content-center">
+                                            <div class="col-lg-12 mx-auto">
+
+                                                <img src="{{ URL::asset('/assets/images/cover/2.jpg') }}"
+                                                    class="bannerImage" alt="Maria Jones">
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- item tns-ite  end -->
+                                    <div class="item tns-item" aria-hidden="true" tabindex="-1">
+                                        <div class="row justify-content-center">
+                                            <div class="col-lg-12 mx-auto">
+
+                                                <img src="{{ URL::asset('/assets/images/cover/2.jpg') }}"
+                                                    class="bannerImage" alt="Maria Jones">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- item tns-ite  end -->
                                 </div>
                             </div>
                         </div>
-
                     </div>
+
                 </div>
             </div>
+
         </div>
+
     </div>
     <!-- End Hero Section -->
 
