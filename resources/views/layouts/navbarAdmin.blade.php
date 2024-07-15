d8db124414840013a374d6e78bb5cba77f4ed77c
THANETNove <novel2020manga@gmail.com>
2024-07-15 22:38:48 +0700

    add

--- resources/views/layouts/navbarAdmin.blade.php
+++ resources/views/layouts/navbarAdmin.blade.php
@@ -24,27 +24,38 @@
     <nav class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
         <div class="container-fluid">
             <nav class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <button type="submit" class="btn btn-search pe-1">
-                            <i class="fa fa-search search-icon"></i>
-                        </button>
+                <form action="">
+                    <div class="input-group">
+                        <input type="text" placeholder="Search ..." class="form-control" />
+                        <div class="input-group-prepend">
+                            <button type="submit" class="btn" style="background-color: #1a2035 ">
+                                <i class="fa fa-search search-icon" style="color: #ffffff"></i>
+                            </button>
+                        </div>
+
                     </div>
-                    <input type="text" placeholder="Search ..." class="form-control" />
-                </div>
+                </form>
             </nav>
 
-            <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
+            <ul class="navbar-nav topbar-nav ms-md-auto align-items-center ">
                 <li class="nav-item topbar-icon dropdown hidden-caret d-flex d-lg-none">
                     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
                         aria-expanded="false" aria-haspopup="true">
                         <i class="fa fa-search"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-search animated fadeIn">
-                        <form class="navbar-left navbar-form nav-search">
-                            <div class="input-group">
-                                <input type="text" placeholder="Search ..." class="form-control" />
+                    <ul class="dropdown-menu dropdown-search animated fadeIn" style="padding-left: 8px ;">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-9">
+                                    <input type="text" placeholder="Search ..." class="form-control" />
+                                </div>
+                                <div class="col-3">
+                                    <button type="submit" class="btn" style="background-color: #1a2035">
+                                        <i class="fa fa-search search-icon" style="color: #ffffff"></i>
+                                    </button>
+                                </div>
                             </div>
+
                         </form>
                     </ul>
                 </li>
