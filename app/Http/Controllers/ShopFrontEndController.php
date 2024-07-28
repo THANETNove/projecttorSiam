590f6a0550a54bdf14c45fec0f623ce633f0cddc
THANETNove <novel2020manga@gmail.com>
2024-07-28 09:20:23 +0700

    add

--- app/Http/Controllers/ShopFrontEndController.php
+++ app/Http/Controllers/ShopFrontEndController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
-use Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\Models\ShoppingList;
 use App\Mail\ExampleMail;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 
 use Illuminate\Support\Facades\Session;
 
@@ -20,54 +20,54 @@ class ShopFrontEndController extends Controller
     public function index($name)
     {
 
-    
+
 
         $data = DB::table('products')
-        ->whereRaw('JSON_CONTAINS(check_manu, ?)', [json_encode($name)])
-        ->where('status_sell', "on")
-        ->orderBy('id', 'DESC')
-        ->paginate(100)
-        ->appends(['name' => $name]);
+            ->whereRaw('JSON_CONTAINS(check_manu, ?)', [json_encode($name)])
+            ->where('status_sell', "on")
+            ->orderBy('id', 'DESC')
+            ->paginate(100)
+            ->appends(['name' => $name]);
 
 
         $brands = DB::table('products')
-        ->whereNotNull('brand' )
-        ->where('status_sell', "on")
-        ->orderBy('id', 'DESC')
-        ->select('brand') // เพิ่ม category
-        ->groupBy('brand')
-        ->get();
+            ->whereNotNull('brand')
+            ->where('status_sell', "on")
+            ->orderBy('id', 'DESC')
+            ->select('brand') // เพิ่ม category
+            ->groupBy('brand')
+            ->get();
 
- 
- 
 
-        return view('frontEndWeb.shop' ,['data' => $data, 'brands' => $brands]);
+
+
+        return view('frontEndWeb.shop', ['data' => $data, 'brands' => $brands]);
     }
 
-    
+
     public function filterByBrand($name)
     {
 
 
         $data = DB::table('products')
-        ->where('brand', $name)
-        ->where('status_sell', "on")
-        ->orderBy('id', 'DESC')
-        ->paginate(100)
-        ->appends(['name' => $name]);
+            ->where('brand', $name)
+            ->where('status_sell', "on")
+            ->orderBy('id', 'DESC')
+            ->paginate(100)
+            ->appends(['name' => $name]);
 
 
         $brands = DB::table('products')
-        ->whereNotNull('brand' )
-        ->where('status_sell', "on")
-        ->orderBy('id', 'DESC')
-        ->groupBy('brand')
-        ->get();
+            ->whereNotNull('brand')
+            ->where('status_sell', "on")
+            ->orderBy('id', 'DESC')
+            ->groupBy('brand')
+            ->get();
 
- 
- 
 
-        return view('frontEndWeb.shop' ,['data' => $data, 'brands' => $brands]);
+
+
+        return view('frontEndWeb.shop', ['data' => $data, 'brands' => $brands]);
     }
 
     /**
@@ -81,13 +81,23 @@ class ShopFrontEndController extends Controller
     {
 
 
-    
+
         if (Auth::check()) {
             $data = DB::table('users')
                 ->where('users.id', Auth::user()->id)
                 ->leftJoin('addresses', 'users.id', '=', 'addresses.user_id')
-                ->select('users.*', 'addresses.phone', 'addresses.fax', 'addresses.company', 'addresses.address_1', 'addresses.address_2',
-                    'addresses.state_city', 'addresses.postal_zip', 'addresses.country', 'addresses.region')
+                ->select(
+                    'users.*',
+                    'addresses.phone',
+                    'addresses.fax',
+                    'addresses.company',
+                    'addresses.address_1',
+                    'addresses.address_2',
+                    'addresses.state_city',
+                    'addresses.postal_zip',
+                    'addresses.country',
+                    'addresses.region'
+                )
                 ->orderBy('users.id', 'DESC')
                 ->get();
         } else {
@@ -95,8 +105,8 @@ class ShopFrontEndController extends Controller
             $data = collect(); // Return an empty collection or handle as needed
         }
 
-        
-        return view('frontEndWeb.buyNow',['data' => $data]);
+
+        return view('frontEndWeb.buyNow', ['data' => $data]);
     }
 
     /**
@@ -118,25 +128,25 @@ class ShopFrontEndController extends Controller
             'region' => ['required', 'string', 'max:255'],
         ]);
 
-        
+
         $items = json_decode($request->itemCart[0], true);
 
         // ตรวจสอบว่าการแปลง JSON สำเร็จ
         if (json_last_error() !== JSON_ERROR_NONE) {
             return response()->json(['error' => 'Invalid JSON'], 400);
         }
-    
+
         // คำนวณผลรวมทั้งหมดของ totalPrice
         $totalPriceSum = array_reduce($items, function ($sum, $item) {
             return $sum + $item['totalPrice'];
         }, 0);
-    
+
 
         $priceSum =  number_format($totalPriceSum);
         // แสดงผลรวมทั้งหมด
 
         $data = new ShoppingList;
-        
+
         $data->itemCart = json_encode($items);
         $data->totalPrice = $totalPriceSum;
         $data->fname = $request['fname'];
@@ -153,12 +163,12 @@ class ShopFrontEndController extends Controller
         $data->region = $request['region'];
         $data->save();
 
-       // $mail = 'siammp@hotmail.co.th';
+        // $mail = 'siammp@hotmail.co.th';
         $mail = 'Projectorsiam@gmail.com';
-         
+
         Mail::to($request['email'])->send(new ExampleMail($data));
-       //Mail::to($mail)->send(new ExampleMail($data));
-        return view('emails.succeedSendEmail',['data' => $data]);
+        //Mail::to($mail)->send(new ExampleMail($data));
+        return view('emails.succeedSendEmail', ['data' => $data]);
     }
 
     /**
@@ -168,30 +178,30 @@ class ShopFrontEndController extends Controller
     {
 
         $data = DB::table('products')
-        ->where('id', $id)
-        ->orderBy('id', 'DESC')
-        ->get();
+            ->where('id', $id)
+            ->orderBy('id', 'DESC')
+            ->get();
 
 
-     // แปลงค่า check_manu จาก JSON string เป็น array
-    $check_manu_array = json_decode($data[0]->check_manu);
+        // แปลงค่า check_manu จาก JSON string เป็น array
+        $check_manu_array = json_decode($data[0]->check_manu);
 
-    // เริ่มต้น query
-    $query = DB::table('products')
-        ->where('status_sell', "on")
-        ->orderBy('id', 'DESC');
+        // เริ่มต้น query
+        $query = DB::table('products')
+            ->where('status_sell', "on")
+            ->orderBy('id', 'DESC');
 
-    // เพิ่มเงื่อนไขสำหรับแต่ละค่าใน array
-    foreach ($check_manu_array as $check_manu) {
-        $query->orWhereRaw('JSON_CONTAINS(check_manu, ?)', [json_encode($check_manu)]);
-    }
+        // เพิ่มเงื่อนไขสำหรับแต่ละค่าใน array
+        foreach ($check_manu_array as $check_manu) {
+            $query->orWhereRaw('JSON_CONTAINS(check_manu, ?)', [json_encode($check_manu)]);
+        }
 
-    // จำกัดผลลัพธ์ให้เหลือ 3 รายการ
-    $dataSlid = $query->take(9)->get();
+        // จำกัดผลลัพธ์ให้เหลือ 3 รายการ
+        $dataSlid = $query->take(9)->get();
 
-    // แสดงผลข้อมูล
-   
-    return view('frontEndWeb.particulars' ,['data' => $data, 'dataSlid' => $dataSlid]);
+        // แสดงผลข้อมูล
+
+        return view('frontEndWeb.particulars', ['data' => $data, 'dataSlid' => $dataSlid]);
     }
 
     /**
